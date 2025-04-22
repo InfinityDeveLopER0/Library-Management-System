ae32c76bc433f58655edab6c55cbb1763ffb2aed
Sanjeev N <worldsansgamming@gmail.com>
2025-04-22 22:35:41 +0530

    Feat: Implemented Concurrecy Control

---
+++ concurrency.sql
@@ -0,0 +1,36 @@
+-- Concurrency Control
+
+--setting the Isolation Level
+SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+
+--Transactions for Critical Operations
+--Book Issue Transaction
+
+START TRANSACTION;
+
+UPDATE Books
+SET QuantityAvailable = QuantityAvailable - 1
+WHERE BookID = 15 AND QuantityAvailable > 0;
+
+INSERT INTO Transactions (BookID, MemberID, IssueDate, DueDate, FineAmount, IssuedByStaffID)
+VALUES (15, 2, CURDATE(), DATE_ADD(CURDATE(), INTERVAL 14 DAY), 0.00, 5);
+
+COMMIT;
+
+--If any step fails:
+ROLLBACK;
+
+
+--Apply Row-Level Locking Where Needed. To prevent race conditions
+
+START TRANSACTION;
+
+SELECT QuantityAvailable FROM Books
+WHERE BookID = 11 FOR UPDATE;
+
+-- Check QuantityAvailable, then issue...
+
+COMMIT;
+
+
+--Recovery Mechanisms
